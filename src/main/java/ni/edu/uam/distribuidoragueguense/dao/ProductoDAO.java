fca12ca345318e14a23fab4f320d41d647b25bc8
cerf007 <cerf02062007@gmail.com>
2026-09-03 16:16:17 -0600

    Implementacion de los métodos CRUD en el data access object

--- src/main/java/ni/edu/uam/distribuidoragueguense/dao/ProductoDAO.java
+++ src/main/java/ni/edu/uam/distribuidoragueguense/dao/ProductoDAO.java
@@ -1,4 +1,60 @@
 package ni.edu.uam.distribuidoragueguense.dao;
 
-public class ProductoDAO {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import ni.edu.uam.distribuidoragueguense.interfaces.Crud;
+import ni.edu.uam.distribuidoragueguense.model.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoDAO implements Crud<Producto, String> {
+
+    private final ObservableList<Producto> listaProductos;
+
+    public ProductoDAO() {
+        this.listaProductos = FXCollections.observableArrayList();
+    }
+
+    public ObservableList<Producto> getListaObservable() {
+
+        return listaProductos;
+    }
+
+    @Override
+    public void agregar(Producto producto) {
+
+        listaProductos.add(producto);
+    }
+
+    @Override
+    public List<Producto> obtenerRegistros() {
+
+        return listaProductos;
+    }
+
+    @Override
+    public Optional<Producto> buscarPorId(String codigo) {
+        return listaProductos.stream()
+                .filter(p -> p.getCodigo().equalsIgnoreCase(codigo))
+                .findFirst();
+    }
+
+    @Override
+    public boolean actualizar(Producto productoEditado) {
+        for (int i = 0; i < listaProductos.size(); i++) {
+            if (listaProductos.get(i).getCodigo().equalsIgnoreCase(productoEditado.getCodigo())) {
+                listaProductos.set(i, productoEditado);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean eliminar(String codigo) {
+        return listaProductos.removeIf(p -> p.getCodigo().equalsIgnoreCase(codigo));
+    }
+
+
 }
